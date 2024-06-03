f43b036aba8a3ec8c41d3cfa22d1761160820ba9
Shun Yuan <tanshunyuan99@gmail.com>
2024-06-03 17:00:33 +0800

    chore: refactor

--- src/hooks/use-setup-session-management.ts
+++ src/hooks/use-setup-session-management.ts
@@ -1,42 +1,61 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import { useUser } from "@clerk/nextjs";
+import { useClerk, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { pusherClient } from "~/lib/pusher/client";
+import { ROUTE_PATHS } from "~/utils/route-paths";
 
 
-let didInitOne = false;
-export const useSetupSessionChannel = () => {
-  const [isReload, setIsReload] = useState(false);
-  const [excessSessionIds, setExcessSessionIds] = useState<string[]>([]);
+const useHandleSignOut = () => {
+  const router = useRouter();
+  return () => router.push(`${ROUTE_PATHS.SIGNIN}?forcedRedirect=true`);
+};
+
+const useHandleSessionRemoval = () => {
+  const { signOut, session: currentSession } = useClerk();
+  const handleSignOut = useHandleSignOut();
+
+  return async (excessSessionIds: string[]) => {
+    try {
+      const hasExcess = excessSessionIds.length > 0;
+      const isCurrentSessionExcess =
+        hasExcess && currentSession && excessSessionIds.includes(currentSession.id);
+
+      if (!isCurrentSessionExcess) return;
+
+      await signOut(handleSignOut, {
+        sessionId: currentSession.id,
+      });
+    } catch (error) {
+      console.error('Error removing session:', error);
+    }
+  };
+};
+
+let didInit = false;
+export const useSubscribeToSessionChannel = () => {
   const { user } = useUser();
+  const handleSessionRemoval = useHandleSessionRemoval();
 
   useEffect(() => {
-    if (!didInitOne) {
+    if (!didInit) {
       const channel = pusherClient
         .subscribe("private-session")
         .bind(
           `evt::revoke-${user?.id}`,
           (data: { type: string; data: string[] }) => {
             if (data.type === "session-revoked") {
-              setIsReload(true);
-              setExcessSessionIds(data.data);
+              handleSessionRemoval(data.data)
             }
           },
         );
-      didInitOne = true;
+      didInit = true;
 
       return () => {
         channel.unbind();
-        didInitOne = false;
+        didInit = false;
       };
     }
-  }, [user]);
-
-  return {
-    isReload,
-    setIsReload,
-    excessSessionIds,
-  };
+  }, [user, handleSessionRemoval]);
 };
-
--- src/utils/app-provider.tsx
+++ src/utils/app-provider.tsx
@@ -1,20 +1,13 @@
 'use client'
-import { useRemoveExcessSessions } from "~/hooks/use-remove-excess-sessions";
-import { useSetupSessionChannel } from "~/hooks/use-setup-session-management";
+import { useSubscribeToSessionChannel } from "~/hooks/use-setup-session-management";
 import { api } from "~/trpc/react";
 import { type BaseChildrenProps } from "~/types/common";
 
 export const AppProvider = (props: BaseChildrenProps) => {
   const { children } = props
 
-  const { excessSessionIds, isReload, setIsReload } = useSetupSessionChannel();
+  useSubscribeToSessionChannel();
   const excessSessionQuery = api.user.getExcessSessions.useQuery()
-
-  useRemoveExcessSessions({
-    excessSessionIds,
-    isReload,
-    setIsReload,
-  });
 
   if (excessSessionQuery.isLoading) return <p>Loading...</p>
 
